04d744b8022319d1562f9324097e4139c974d424
Amin070204 <145484915+Amin070204@users.noreply.github.com>
2024-11-14 17:08:14 -0600

    Inicio y nosotros

--- vista/Nosotros.php
+++ vista/Nosotros.php
@@ -1,17 +1,39 @@
-<?php include ("layout/header.php"); ?>
+<?php include("layout/header.php"); ?>
 
-<h1>Nosotros</h1><br>
-<h1>lashkjalsjañlskalskalk</h1>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br><br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<?php require("layout/footer.php");?>
+<article class="nosotros_article">
+    <div class="nosotros_banner">
+
+    </div>
+    <div class="nosotros_info">
+        <div class="nosotros_titulo">
+            <h1 align="center">Explorando Chiapas</h1>
+        </div>
+        <div class="nosotros_cont">
+            <h3 align="center">Operadora Turística Selva Azul Chiapas S. DE R.L DE C.V</h3>
+            <p align="center">Tel:(967 555 93 32) <br>
+                Cel:(967 126 45 54- 967 581 68 77) <br>
+                Correo: explorando.chiapas@gmail.com <br>
+                Av Centran entre 6ta y 7ma pte <br> <br>
+                Nota: <br>
+                Para poder Realizar la Confirmación de su Reservaciòn tendrá que Realizar el deposito o Transferencia del 20% O hasta en 50% de la cantidad Total a Pagar
+                dependiendo del paquete, el resto se le cobrará personalmente llegando a Chiapas.
+            </p>
+            <div class="nosotros_maps">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3794.2945185318326!2d-93.1222365!3d16.7542332!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ecd8f3ea1ccec5%3A0x70614761d0ddd713!2sAv%20Central%20Pte%20761%2C%20Guadalupe%2C%2029066%20Tuxtla%20Guti%C3%A9rrez%2C%20Chis.!5e0!3m2!1ses-419!2smx!4v1696700000000!5m2!1ses-419!2smx"
+                    width="600"
+                    height="450"
+                    style="border:0;"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+            </div>
+        </div>
+    </div>
+</article>
+
+
+
+
+<?php require("layout/footer.php"); ?>
--- vista/index.php
+++ vista/index.php
@@ -1,17 +1,38 @@
-<?php include ("layout/header.php"); ?>
+<?php include("layout/header.php"); ?>
 
-<h1>Inicio</h1><br>
-<h1>lashkjalsjañlskalskalk</h1>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br><br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<?php require("layout/footer.php");?>
+<article class="inicio_article">
+    <div class="inicio_bienvenida">
+        <div class="inicio_bienvenidainfo">
+            <h1>Será un placer atenderte</h1>
+            <p>En Explorando Chiapas te asesoramos para seleccionar el mejor paquete para sus próximas vacaciones, el profesionalismo y buena atención nos caracterizan.</p>
+            <div class="inicio_toncluido">
+                <div class="inicio_toncluido_info">
+                    <p><b>Paquetes todo incluido</b><br>Conoce todo Chiapas con nuestros mejores paquetes.</p>
+                </div>
+            </div>
+            <div class="inicio_hotel">
+                <div class="inicio_hotel_info">
+                    <p><b>Los mejores hoteles</b><br>Te ofrecemos los hoteles llenos de seguridad.</p>
+                </div>
+            </div>
+            <div class="inicio_expunicas">
+                <div class="inicio_expunicas_info">
+                    <p><b>Experiencias unicas</b><br>Vive con nosotros las mejores experiencias.</p>
+                </div>
+            </div>
+            <div class="inicio_asesoria">
+                <div class="inicio_asesoria_info">
+                    <p><b>Asesoria personalizada</b><br>Nuestros agentes te ayudaran a tomar la mejor decision.</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="inicio_imgbienvenida">
+
+        </div>
+    </div>
+    <div class="inicio_banner">
+    </div>
+</article>
+
+<?php require("layout/footer.php"); ?>
